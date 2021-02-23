2223e14a5686732d6fa9f2709ec376a54fed9e64
rileybarry <60715866+rileybarry@users.noreply.github.com>
2021-02-22 23:40:02 -0600

    final update

--- forms/pets/pets.js
+++ forms/pets/pets.js
@@ -1,8 +1,16 @@
+//c
 let petNames = ['Darcy','Jack','Jerry','Marty','Vito','Vinny','Riley','Saddie','Marge','Sam']
 
+//d
 let withSal = petNames
+//e
 withSal.push('Sal')
 
-//question asks for "petsArray" (assumed "petNames" array)
-console.log(`${petNames[2]}`)
+//f question asks for "petsArray" (assumed "withSal" array)
+console.log(`The third pet is named ${withSal[2]}.`)
 
+//g
+console.log(`The array has ${withSal.length} names in it.`)
+
+//h
+withSal.pop(10)
