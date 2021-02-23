fab95a071b73640c9c5a1c1ebe9c216387f92f74
rileybarry <60715866+rileybarry@users.noreply.github.com>
2021-02-22 23:29:20 -0600

    First commit

---
+++ forms/pets/pets.js
@@ -0,0 +1,8 @@
+let petNames = ['Darcy','Jack','Jerry','Marty','Vito','Vinny','Riley','Saddie','Marge','Sam']
+
+let withSal = petNames
+withSal.push('Sal')
+
+//question asks for "petsArray" (assumed "petNames" array)
+console.log(`${petNames[2]}`)
+
